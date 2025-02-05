3793b470f00cc88ec2450abe05170f7b464e60be
Juan David Sandoval <judsandovalca@unal.edu.co>
2025-02-04 23:03:31 -0500

    bases of graphql

--- services/users/src/app.module.ts
+++ services/users/src/app.module.ts
@@ -3,14 +3,17 @@ import { AppController } from './app.controller';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { GraphQLModule } from '@nestjs/graphql';
 import { AppService } from './app.service';
+import { join } from 'path';
+import { FooResolver } from './providers/resolver';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, FooResolver],
 })
 export class AppModule {}
